636bf6c2e588a2e84f975e17466808465cfc3b36
ny248 <dope148@choco.la>
2023-04-04 18:10:00 +0900

    Added class point

--- canvas.py
+++ canvas.py
@@ -1,19 +1,50 @@
 import math, numpy
 import tkinter as tk
 from functools import partial
-global canvas, command, root
-def enter_command(value):
-    global command
-    print(command.get())
+global canvas, command, root, objects
 
+class point:
+    global canvas, object
+    def __init__(self, x, y):
+        self.coordinate = (x, y)
+        id = canvas.create_oval(x - 2, y - 2, x + 2, y + 2, fill = "black")
+        objects.add(id)
+        draw_shelf()
+def draw_shelf():
+    global shelf
+def is_num(s):
+    try:
+        float(s)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def enter_command(event):
+    global command, objects
+    com = command.get().split()
+    if com[0] == "point":
+        if len(com) < 3:
+            print("Too few values.")
+            return
+        pos = []
+        for i in range(1, 3):
+            if not is_num(com[i]):
+                print("Not a number.")
+                return
+            pos.append(float(com[i]))
+        point(pos[0], pos[1])
+    else:
+        print("Unknown command.")
 def create_GUI():
-    global canvas, command, root
+    global canvas, command, root, objects, shelf
+    objects = set()
     root = tk.Tk()
     root.title("GUI")
     root.geometry("600x500")
     root.configure(bg = "white")
-    objects = tk.Frame(root)
-    objects.place(x = 0, y = 0, width = 80, relheight = 0.95)
+    shelf = tk.Frame(root)
+    shelf.place(x = 0, y = 0, width = 80, relheight = 0.95)
     canvas = tk.Canvas(root)
     canvas.place(x = 80, y = 0, relwidth = 1, relheight = 0.95)
     canvas.configure(bg = "#ffffee")
